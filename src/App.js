95ae0a2750580d239bddf7e8d944d7135077e4c2
zdiehlio <zachary.diehl@gmail.com>
2018-11-26 09:08:04 -0500

    finished parallax

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Logo from './assets/other_logo.png'
+import Tab from '@material-ui/core/Tab';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css';
+import Home from './Home'
+import About from './About'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="parallax-container">
+            <div className="parallax"></div>
+          </div>
+          <div className="main-container">
+            <Link to='/'><Tab label="Home"/></Link>
+            <Link to='/about'><Tab label="About"/></Link>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <div className='content'></div>
+            <div className='content'></div>
+            <div className='content'></div>
+
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+    return(
+        <p>This is home</p>
+    )
+}
+
+export default Home
